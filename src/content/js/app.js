187d3b91e6fa2f16c65f758bf3afdf6a32bd54bd
Victor Gomes <victor.silva@mult-e.com.br>
2019-01-25 11:01:27 -0200

    Correções e criação do botão scroll top

--- src/content/js/app.js
+++ src/content/js/app.js
@@ -1,14 +1,28 @@
-// PROGRESSBAR NIVEIS
-const progressbar = document.querySelector('.progress');
-range.addEventListener('input', function(){
-	const value = range.value;
-    progressbar.style.setProperty('--progress', value)
+// Active Item Menu
+$(document).on('click', '.menu-content-a', function () {
+    $('.sub-menu').removeClass('in');
 });
 
-// COLLAPSE MENU
-// Active Item Menu
-var menuItem = document.querySelectorAll(".menu-item");
+$(document).on('click', 'body', function () {
+    $('.sub-menu').removeClass('in');
+});
 
-menuItem.addEventListener('click', function () {
-    alert("CLIQUEI!");
-})
+
+// Scroll Top
+$(document).ready(function(){
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300) {
+            $('.button-scrollToTop').fadeIn();
+        } else {
+            $('.button-scrollToTop').fadeOut();
+        }
+    });
+    
+    //Click event to scroll to top
+    $('.button-scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+    
+});
